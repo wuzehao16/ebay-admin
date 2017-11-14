fa6b2261b02005ee10c46955bf0ac9647d76fd5e
aisic <wuzehao16@qq.com>
2017-11-14 18:59:52 +0800

    ebay

--- src/components/order/add.vue
+++ src/components/order/add.vue
@@ -45,7 +45,7 @@
           <el-row type="flex" class="row-bg" justify="center" :gutter='20'>
             <el-col :span='8'>
               <el-form-item label='物流费用：'>
-                <el-input v-model.number="orderInfo.carriageFee" placeholder="物流费用">
+                <el-input v-model.number="orderInfo.carriage" placeholder="物流费用">
                   <template slot="append">元</template>
                 </el-input>
               </el-form-item>
@@ -188,7 +188,7 @@ export default {
         user_type: 0,
         create_time: "",
         goods_tax: 0,
-        carriageFee: 0,
+        carriage: 0,
         logisticsStatus: 0,
         name: "",
         cneePhone: "",
@@ -218,7 +218,7 @@ export default {
   },
   methods: {
     handleAddressChange(val) {
-      // 
+      //
     },
     addressCodeToaddress() {
       if (
@@ -236,7 +236,7 @@ export default {
     },
     addSubmit() {
       this.addressCodeToaddress();
-      
+
       reqAddOrder(this.orderInfo)
         .then(res => {
           if (res.data.code == 0) {
@@ -253,7 +253,7 @@ export default {
           }
         })
         .catch(function(error) {
-          
+
         });
     },
     resetForm(formName) {
@@ -287,7 +287,7 @@ export default {
         return newArray
     },
     handleSelectphone(item){
-      
+
       this.orderInfo.openid = item.userWxOpenid
     },
     //远程拉取商品名列表
@@ -314,7 +314,7 @@ export default {
       return newArray;
     },
     handleSelect(item) {
-      
+
       this.orderInfo.items[0].productId = item.id;
       this.orderInfo.items[0].productName = item.value;
     }
@@ -326,7 +326,7 @@ export default {
       ).toFixed(2);
     },
     calOrderTotalPrice() {
-      return (parseFloat(this.calGoodsTotalPrice) + this.orderInfo.carriageFee
+      return (parseFloat(this.calGoodsTotalPrice) + this.orderInfo.carriage
       ).toFixed(2);
     }
   },
